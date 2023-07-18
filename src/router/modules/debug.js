d3379b4f98818617d0540eb3701158a1f024d807
雨霖铃 <3415189469@qq.com>
2023-07-18 23:42:37 +0800

    modify

---
+++ src/router/modules/debug.js
@@ -0,0 +1,5 @@
+export default {
+    path: 'test',
+    component: () => import('@/views/debug/Test.vue'),
+    children: []
+}
--- src/router/modules/test.js
+++
@@ -1,4 +0,0 @@
-export default {
-    path: '/test',
-    component: () => import('@/views/debug/Test.vue')
-}
