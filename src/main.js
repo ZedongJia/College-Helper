ac86d79463073755b2709df61a90b31f270f2607
ZD_J <2698132782@qq.com>
2023-07-28 13:21:42 +0800

    修改个人中心主页，下载ionicon包

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from './router'
 import store from './store'
 import * as echarts from 'echarts'
 import akComponents from './lib/index'
-import 'ionicons/dist/ionicons/ionicons.esm.js'
-import 'ionicons/dist/ionicons/ionicons.js'
 const app = createApp(App)
 app.config.globalProperties.echarts = echarts
 app.use(akComponents)
