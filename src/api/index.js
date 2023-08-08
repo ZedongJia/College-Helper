5361b96394bc0e955730c9c648c8a58fe2e967e1
ZD_J <2698132782@qq.com>
2023-08-08 22:12:10 +0800

    打包测试，添加配置

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ const STATE = {
     NOT_FOUND: 400
 }
 axios.defaults.baseURL = 'http://localhost:8000/api'
+// axios.defaults.baseURL = 'http://8.130.10.241/api'
 
 axios.defaults.withCredentials = true
 
